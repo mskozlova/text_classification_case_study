8efe360711f9381e25465f74875e57e089cee800
mskozlova <be-more-lavender@yandex.ru>
2023-11-24 22:31:15 +0000

    corpus dictionary - add indexers

--- modelling/util.py
+++ modelling/util.py
@@ -24,3 +24,31 @@ class CorpusDictionary:
         
         self.word_to_idx, self.idx_to_word = self._create_indexers(unknown_symbol, pad_symbol)
 
+  
+    def _create_dictionary(self, tokenized_texts):
+        dictionary = defaultdict(int)
+        for tokens in tokenized_texts:
+            tokens = set(tokens)
+            for token in tokens:
+                dictionary[token] += 1
+        
+        return dictionary
+
+
+    def _create_indexers(self, unknown_symbol, pad_symbol):
+        word_to_idx = {word: idx for word, idx in zip(self.dictionary.keys(), range(2, len(self.dictionary) + 2))}
+        word_to_idx[pad_symbol] = 0
+        word_to_idx[unknown_symbol] = 1
+
+        idx_to_word = {idx: word for word, idx in word_to_idx.items()}
+        
+        return word_to_idx, idx_to_word
+    
+    
+    def get_frequencies(self):
+        return sorted(
+            [(word, frequency / self.n_texts) for word, frequency in self.dictionary.items()],
+            key=lambda x: x[1]
+        )
+    
+    
